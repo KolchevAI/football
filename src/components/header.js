fbe86d9dc6e8f367bdca71a3aeddb12227167b14
Alex Dechart <alex.arrow26@gmail.com>
2024-05-18 21:39:05 +0300

    del and change

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>he hey sports</span>
+        </div>
+        <nav>
+            <a href="index.html">Первенство</a>
+            <a href="teams.html">Команды</a>
+            <a href="stadiums.html">Стадионы</a>
+            <a href="organizers.html">Организаторам</a>
+        </nav>
+    </header>
+  )
+}
